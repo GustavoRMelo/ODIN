97e920a52dc9ad599d409e635584847eb5d2e4c0
Gustavo Rezende Melo <gustavorezendemelo@gmail.com>
2021-06-27 16:08:39 -0300

    Exercicio hamster

---
+++ hamster.js
@@ -0,0 +1,18 @@
+let hamster= {
+	stomach: [],
+
+	eat(food){
+		this.stomach.push(food);
+	}
+};
+
+let speedy = {
+	__proto__: hamster,
+	stomach: []
+};
+
+let lazy = {
+	__proto__:hamster,
+	stomach: []
+};
+
